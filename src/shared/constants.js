d9f02f4ffe88b055c9eb9433b46c2111534f76f7
Brian Chao <whereisbrianchao520@gmail.com>
2019-09-08 18:06:53 +0800

    first commit

---
+++ src/shared/constants.js
@@ -0,0 +1,24 @@
+module.exports = Object.freeze({
+  PLAYER_RADIUS: 20,
+  PLAYER_MAX_HP: 100,
+  PLAYER_SPEED: 400,
+  PLAYER_FIRE_COOLDOWN: 0.25,
+
+  BULLET_RADIUS: 3,
+  BULLET_SPEED: 800,
+  BULLET_DAMAGE: 10,
+
+  SCORE_BULLET_HIT: 20,
+  SCORE_PER_SECOND: 1,
+
+  MAP_SIZE: 500,
+  MSG_TYPES: {
+    JOIN_GAME: 'join_game',
+    GAME_UPDATE: 'update',
+    INPUT: 'input',
+    GAME_OVER: 'dead',
+  },
+  MONITOR_PIXEL_OFFSET: 2,
+  MONITOR_SIZE: 100,
+  MONITOR_MARGIN: 10,
+});
