140b60aec8bd4a371501d9d48e9ecbab32b3832e
tzyLee <b06901145@ntu.edu.tw>
2019-09-09 13:37:18 +0800

    Buggy pixi render

--- src/client/render.js
+++ src/client/render.js
@@ -3,46 +3,98 @@
 import { debounce } from 'throttle-debounce';
 import { getAsset } from './assets';
 import { getCurrentState } from './state';
+import * as PIXI from 'pixi.js';
 
 const Constants = require('../shared/constants');
 
-const { PLAYER_RADIUS, PLAYER_MAX_HP, BULLET_RADIUS, MAP_SIZE, MONITOR_PIXEL_OFFSET, MONITOR_SIZE, MONITOR_MARGIN } = Constants;
+const {
+  PLAYER_RADIUS,
+  PLAYER_MAX_HP,
+  BULLET_RADIUS,
+  MAP_SIZE,
+  MONITOR_PIXEL_OFFSET,
+  MONITOR_SIZE,
+  MONITOR_MARGIN
+} = Constants;
 
+const scaleRatio = Math.max(1, 800 / window.innerWidth);
 // Get the canvas graphics context
-const canvas = document.getElementById('game-canvas');
-const context = canvas.getContext('2d');
-setCanvasDimensions();
+let app = new PIXI.Application({
+  width: scaleRatio * window.innerWidth,
+  height: scaleRatio * window.innerHeight
+});
+// app.renderer.backgroundColor = 0xffffff;
+const canvas = app.view;
+document.body.appendChild(canvas);
+app.renderer.clearBeforeRender = true;
 
-function setCanvasDimensions() {
-  // On small screens (e.g. phones), we want to "zoom out" so players can still see at least
-  // 800 in-game units of width.
-  const scaleRatio = Math.max(1, 800 / window.innerWidth);
-  canvas.width = scaleRatio * window.innerWidth;
-  canvas.height = scaleRatio * window.innerHeight;
-}
+window.addEventListener(
+  'resize',
+  debounce(40, () => {
+    app.view.width = scaleRatio * window.innerWidth;
+    app.view.height = scaleRatio * window.innerHeight;
+  })
+);
 
-window.addEventListener('resize', debounce(40, setCanvasDimensions));
+let textures = {
+  bullet: PIXI.Texture.from('assets/bullet.svg'),
+  ship: PIXI.Texture.from('assets/ship.svg')
+};
+
+let shipSprites = {};
+let bulletSprites = {};
 
 function render() {
-  const { me, others, bullets } = getCurrentState();
+  const { me, others, bullets, ...rest } = getCurrentState();
   if (!me) {
     return;
   }
+  prepareSprite(shipSprites, [me], textures.ship);
+  prepareSprite(shipSprites, others, textures.ship);
+  prepareSprite(bulletSprites, bullets, textures.bullet);
+  console.log(Object.keys(bulletSprites).length);
 
-  // Draw background
-  renderBackground(me.x, me.y);
+  // // Draw background
+  // renderBackground(me.x, me.y);
 
-  // Draw boundaries
-  context.strokeStyle = 'black';
-  context.lineWidth = 1;
-  context.strokeRect(canvas.width / 2 - me.x, canvas.height / 2 - me.y, MAP_SIZE, MAP_SIZE);
+  // // Draw boundaries
+  // app.renderer.context.strokeStyle = 'black';
+  // app.renderer.context.lineWidth = 1;
+  // app.renderer.context.strokeRect(
+  //   canvas.width / 2 - me.x,
+  //   canvas.height / 2 - me.y,
+  //   MAP_SIZE,
+  //   MAP_SIZE
+  // );
 
-  // Draw all bullets
+  // // Draw all bullets
   bullets.forEach(renderBullet.bind(null, me));
 
-  // Draw all players
+  // // Draw all players
   renderPlayer(me, me);
   others.forEach(renderPlayer.bind(null, me));
+}
+
+let latestUpdateFlag = true;
+function prepareSprite(spriteHash, objectArray, texture) {
+  const len = objectArray.length;
+  if (!objectArray || !len) return;
+  for (let i = 0; i < len; ++i) {
+    if (!spriteHash[objectArray[i].id]) {
+      spriteHash[objectArray[i].id] = new PIXI.Sprite(texture);
+      app.stage.addChild(spriteHash[objectArray[i].id]);
+    }
+    spriteHash[objectArray[i].id].inUse = latestUpdateFlag;
+  }
+
+  Object.keys(spriteHash).forEach(playerID => {
+    if (spriteHash[playerID].inUse !== latestUpdateFlag) {
+      app.stage.removeChild(spriteHash[playerID]);
+      delete spriteHash[playerID];
+    }
+  });
+
+  latestUpdateFlag = !latestUpdateFlag;
 }
 
 function renderBackground(x, y) {
@@ -54,7 +106,7 @@ function renderBackground(x, y) {
     MAP_SIZE / 10,
     backgroundX,
     backgroundY,
-    MAP_SIZE / 2,
+    MAP_SIZE / 2
   );
   backgroundGradient.addColorStop(0, 'black');
   backgroundGradient.addColorStop(1, 'gray');
@@ -63,68 +115,85 @@ function renderBackground(x, y) {
 }
 
 function renderPlayerOnMonitor(x, y) {
-  const mapToMonitorScale =  MONITOR_SIZE / MAP_SIZE;
-  const [ monitorX, monitorY ] =  Object.freeze([mapToMonitorScale * x, mapToMonitorScale * y]);
+  const mapToMonitorScale = MONITOR_SIZE / MAP_SIZE;
+  const [monitorX, monitorY] = Object.freeze([
+    mapToMonitorScale * x,
+    mapToMonitorScale * y
+  ]);
   const canvasX = MONITOR_MARGIN + monitorX;
   const canvasY = canvas.height - MONITOR_SIZE - MONITOR_MARGIN + monitorY;
-  context.fillStyle = "yellow";
-  context.fillRect(canvasX, canvasY, MONITOR_PIXEL_OFFSET, MONITOR_PIXEL_OFFSET);
+  context.fillStyle = 'yellow';
+  context.fillRect(
+    canvasX,
+    canvasY,
+    MONITOR_PIXEL_OFFSET,
+    MONITOR_PIXEL_OFFSET
+  );
 }
 
 // Renders a ship at the given coordinates
 function renderPlayer(me, player) {
-  const { x, y, direction } = player;
+  const { x, y, direction, id } = player;
   const canvasX = canvas.width / 2 + x - me.x;
   const canvasY = canvas.height / 2 + y - me.y;
+  const ship = shipSprites[id];
+
+  ship.x = canvasX;
+  ship.y = canvasY;
+  ship.anchor.x = 0.5;
+  ship.anchor.y = 0.5;
+  ship.rotation = direction;
 
   // Draw ship
-  context.save();
-  context.translate(canvasX, canvasY);
-  context.rotate(direction);
-  context.drawImage(
-    getAsset('ship.svg'),
-    -PLAYER_RADIUS,
-    -PLAYER_RADIUS,
-    PLAYER_RADIUS * 2,
-    PLAYER_RADIUS * 2,
-  );
-  context.restore();
+  // context.save();
+  // context.translate(canvasX, canvasY);
+  // context.rotate(direction);
+  // context.drawImage(
+  //   getAsset('ship.svg'),
+  //   -PLAYER_RADIUS,
+  //   -PLAYER_RADIUS,
+  //   PLAYER_RADIUS * 2,
+  //   PLAYER_RADIUS * 2
+  // );
+  // context.restore();
 
   // Draw health bar
-  context.fillStyle = 'white';
-  context.fillRect(
-    canvasX - PLAYER_RADIUS,
-    canvasY + PLAYER_RADIUS + 8,
-    PLAYER_RADIUS * 2,
-    2,
-  );
-  context.fillStyle = 'red';
-  context.fillRect(
-    canvasX - PLAYER_RADIUS + PLAYER_RADIUS * 2 * player.hp / PLAYER_MAX_HP,
-    canvasY + PLAYER_RADIUS + 8,
-    PLAYER_RADIUS * 2 * (1 - player.hp / PLAYER_MAX_HP),
-    2,
-  );
+  // context.fillStyle = 'white';
+  // context.fillRect(
+  //   canvasX - PLAYER_RADIUS,
+  //   canvasY + PLAYER_RADIUS + 8,
+  //   PLAYER_RADIUS * 2,
+  //   2
+  // );
+  // context.fillStyle = 'red';
+  // context.fillRect(
+  //   canvasX - PLAYER_RADIUS + (PLAYER_RADIUS * 2 * player.hp) / PLAYER_MAX_HP,
+  //   canvasY + PLAYER_RADIUS + 8,
+  //   PLAYER_RADIUS * 2 * (1 - player.hp / PLAYER_MAX_HP),
+  //   2
+  // );
 
-  renderPlayerOnMonitor(x, y);
+  // renderPlayerOnMonitor(x, y);
 }
 
 function renderBullet(me, bullet) {
-  const { x, y } = bullet;
-  context.drawImage(
-    getAsset('bullet.svg'),
-    canvas.width / 2 + x - me.x - BULLET_RADIUS,
-    canvas.height / 2 + y - me.y - BULLET_RADIUS,
-    BULLET_RADIUS * 2,
-    BULLET_RADIUS * 2,
-  );
+  const { x, y, id } = bullet;
+  bulletSprites[id].x = canvas.width / 2 + x - me.x - BULLET_RADIUS;
+  bulletSprites[id].y = canvas.height / 2 + y - me.y - BULLET_RADIUS;
+  // context.drawImage(
+  //   getAsset('bullet.svg'),
+  //   canvas.width / 2 + x - me.x - BULLET_RADIUS,
+  //   canvas.height / 2 + y - me.y - BULLET_RADIUS,
+  //   BULLET_RADIUS * 2,
+  //   BULLET_RADIUS * 2
+  // );
 }
 
 function renderMainMenu() {
   const t = Date.now() / 7500;
   const x = MAP_SIZE / 2 + 800 * Math.cos(t);
   const y = MAP_SIZE / 2 + 800 * Math.sin(t);
-  renderBackground(x, y);
+  // renderBackground(x, y);
 }
 
 let renderInterval = setInterval(renderMainMenu, 1000 / 60);
