ea73a728b33141ff9406c4e8513ca60b1da8a0d2
MortalHappiness <b07901069@ntu.edu.tw>
2019-09-27 14:50:17 +0800

    add gameover board

--- src/client/index.js
+++ src/client/index.js
@@ -11,23 +11,32 @@ const playMenu = document.getElementById('play-menu');
 const playButton = document.getElementById('play-button');
 const usernameInput = document.getElementById('username-input');
 
+const gameoverBoard = document.getElementById('gameover-board');
+const replayButton = document.getElementById('replay-button');
+const replayUsernameInput = document.getElementById('replay-username-input');
 
 connect(onGameOver)
 .then(() => {
   playMenu.classList.remove('hidden');
   usernameInput.focus();
+  replayUsernameInput.focus();
   // click the playButton or press Enter to start playing
   playButton.onclick = playSetup;
+  replayButton.onclick = replaySetup;
   usernameInput.onkeypress = (e) => {
     // Enter key event code == 13
     if (e.which == 13) { playSetup(); }
+  };
+  replayUsernameInput.onkeypress = (e) => {
+    // Enter key event code == 13
+    if (e.which == 13) { replaySetup(); }
   };
 }).catch(console.error);
 
 function onGameOver() {
   stopCapturingInput();
   stopRendering();
-  playMenu.classList.remove('hidden');
+  gameoverBoard.classList.remove('hidden');
   setLeaderboardHidden(true);
   setBroadcastBoardHidden(true);
 }
@@ -36,6 +45,17 @@ function onGameOver() {
 function playSetup() {
   play(usernameInput.value);
   playMenu.classList.add('hidden');
+  initState();
+  startCapturingInput();
+  startRendering();
+  setLeaderboardHidden(false);
+  setBroadcastBoardHidden(false);
+}
+
+// set up for replaying
+function replaySetup() {
+  play(replayUsernameInput.value);
+  gameoverBoard.classList.add('hidden');
   initState();
   startCapturingInput();
   startRendering();
