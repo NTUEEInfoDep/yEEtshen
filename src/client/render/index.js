f863fe5b3eab78778c8ea913bd54adf353559bae
Kenchu123 <k88913n@gmail.com>
2019-09-28 20:35:50 +0800

    fix

--- src/client/render/index.js
+++ src/client/render/index.js
@@ -45,13 +45,15 @@ const bulletPool = new BulletPool(app);
 const itemPool = new ItemPool(app);
 
 // render button if mobile: todo: check mobile
-if (device.mobile() || device.tablet()) {
-  rightBtn.setpos(canvas.width - 80, canvas.height - 70);
-  leftBtn.setpos(canvas.width - 150, canvas.height - 70);
-  fireBtn.setpos(100, canvas.height - 200);
-  app.stage.addChild(rightBtn.btnSprite);
-  app.stage.addChild(leftBtn.btnSprite);
-  app.stage.addChild(fireBtn.btnSprite);
+function renderButton() {
+  if (device.mobile() || device.tablet()) {
+    rightBtn.setpos(canvas.width - 80, canvas.height - 70);
+    leftBtn.setpos(canvas.width - 150, canvas.height - 70);
+    fireBtn.setpos(100, canvas.height - 200);
+    app.stage.addChild(rightBtn.btnSprite);
+    app.stage.addChild(leftBtn.btnSprite);
+    app.stage.addChild(fireBtn.btnSprite);
+  }
 }
 
 // When playing.
@@ -59,6 +61,8 @@ function render() {
   const { me, others, bullets, items } = getCurrentState();
   if (!me) { return; }
 
+  //render button
+  renderButton();
   // render background
   background.render(me);
 
