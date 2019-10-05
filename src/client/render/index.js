3c6a883221bdad4f8c8480b9531ae49199f0ab9d
Kenchu123 <k88913n@gmail.com>
2019-10-05 14:38:04 +0800

    scale the btn

--- src/client/render/index.js
+++ src/client/render/index.js
@@ -62,9 +62,9 @@ if (isBtn) {
 }
 
 function renderButton() {
-  rightBtn.setpos(canvas.width - 80, canvas.height - 70);
-  leftBtn.setpos(canvas.width - 150, canvas.height - 70);
-  fireBtn.setpos(100, canvas.height - 70);
+  rightBtn.setpos(canvas.width - 100, canvas.height - 70);
+  leftBtn.setpos(canvas.width - 240, canvas.height - 70);
+  fireBtn.setpos(110, canvas.height - 80);
 }
 
 // When playing.
