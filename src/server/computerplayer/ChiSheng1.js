94786d4c8dc1b6eb0271e65e3f5432461e19c737
MortalHappiness <b07901069@ntu.edu.tw>
2019-10-06 21:14:05 +0800

    complete ShiSheng1

--- src/server/computerplayer/ChiSheng1.js
+++ src/server/computerplayer/ChiSheng1.js
@@ -1,15 +1,38 @@
 const ComputerPlayer = require('./computerplayer');
 const Bullet = require('../bullet');
+const { MAP_SIZE } = require('../../shared/constants');
+const { Point } = require('../../shared/utils');
+
+// =====================================
+
+const centralPoint = {x: MAP_SIZE / 2, y: MAP_SIZE / 2};
+
+// =====================================
 
 class ChiSheng1 extends ComputerPlayer {
   constructor(x, y) {
     super('ChiSheng1', '奇聖一號（電腦）', x, y);
     this.fireCooldown = 0.35;
+    this.rotateSpeed = 0;
   }
 
   move() {
-    const randomAngle = (Math.random() * 10) - 5;
-    this.rotateSpeed = Math.round(randomAngle) * Math.PI / 180;
+    // decide whether to change direction
+    const testNum = Math.random();
+    if (testNum < 0.1) {
+      const directionVec = Point.directionToPoint(this.direction);
+      // current point, use left bottom corner as origin
+      const currentPoint = {x: this.x, y: MAP_SIZE - this.y};
+      // current to central vector
+      const curToCenVec = Point.subtract(centralPoint, currentPoint);
+      const randomConstVec = Point.scale(Point.directionToPoint(Math.random() * 2 * Math.PI), 0.4 * MAP_SIZE);
+      const curToCenVec1 = Point.add(curToCenVec, randomConstVec);// add a const vector to it
+      const curToCenUnitVec = Point.unitVector(curToCenVec1);
+
+      const newDirVec = Point.scale(Point.add(Point.scale(directionVec, 3), curToCenUnitVec), 1/4);
+
+      this.direction = Point.direction(newDirVec);
+    }
   }
 
   shot() {
--- src/shared/utils.js
+++ src/shared/utils.js
@@ -11,7 +11,76 @@ function nonBreakingSpaces(str) {
   return str.replace(/ /g, "&nbsp;");
 }
 
+class Point {
+  // The distance between two points
+  static distance(p1, p2) {
+    const dx = (p1.x - p2.x);
+    const dy = (p1.y - p2.y);
+    return Math.sqrt(dx * dx + dy * dy);
+  }
+
+  static length(p) {
+    return Point.distance(p, {x: 0, y: 0});
+  }
+
+  static subtract(p1, p2) {
+    return {x: (p1.x - p2.x), y: (p1.y - p2.y)};
+  }
+
+  static add(p1, p2) {
+    return {x: (p1.x + p2.x), y: (p1.y + p2.y)};
+  }
+
+  // p: point, c: constant
+  static scale(p, c) {
+    return {x: (c * p.x), y: (c * p.y)};
+  }
+
+  // east is 0, north is 0.5*PI, etc.(counterclockwise, unit: rad)
+  static directionAngleX(p) {
+    // First quadrant(and positive x, y axes, origin)
+    if (p.x >= 0 && p.y >= 0) {
+      return Math.atan2(p.y, p.x);
+    }
+    // Second quadrant
+    if (p.x < 0 && p.y > 0) {
+      return (Math.PI - Math.atan2(p.y, -p.x));
+    }
+    // Third quadrant(and negative x, y axes)
+    if (p.x <= 0 && p.y <= 0) {
+      return (Math.PI + Math.atan2(-p.y, -p.x));
+    }
+    // Forth quadrant
+    if (p.x > 0 && p.y < 0) {
+      return (1.5 * Math.PI + Math.atan2(-p.y, p.x));
+    }
+    return 0;
+  }
+
+  // azimuthal angle
+  // north is 0, east is 0.5*PI, etc.(clockwise, unit: rad)
+  static direction(p) {
+    const angleX = Point.directionAngleX(p);
+    const inverseAngleX = (2 * Math.PI - angleX) % (2 * Math.PI);
+    return (inverseAngleX + 0.5 * Math.PI) % (2 * Math.PI);
+  }
+
+  // convert the azimuthal angle into a vector(point) with length one
+  static directionToPoint(dir) {
+    const inverseDir = (2 * Math.PI - dir) % (2 * Math.PI);
+    const angleX = (inverseDir + 0.5 * Math.PI) % (2 * Math.PI);
+    return {x: Math.cos(angleX), y: Math.sin(angleX)};
+  }
+
+  static unitVector(p) {
+    const length = Point.length(p);
+    return Point.scale(p, 1 / length);
+  }
+}
+
+
 module.exports = {
   truncateName,
   nonBreakingSpaces,
+  Point,
 }
