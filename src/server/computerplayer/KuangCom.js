d2d12faa4d4c54a489260f420a272c37037e5096
Kenchu123 <k88913n@gmail.com>
2019-10-06 19:16:44 +0800

    Kuang

---
+++ src/server/computerplayer/KuangCom.js
@@ -0,0 +1,43 @@
+const ComputerPlayer = require('./computerplayer');
+const Bullet = require('../bullet');
+const Constants = require('../../shared/constants');
+
+class KuangCom extends ComputerPlayer {
+  constructor(x, y) {
+    super('KuangCom', '廣哥號', x, y);
+    this.direction = Math.PI / 2;
+    this.speed = 100;
+    this.rotateSpeed = 0;
+    this.shootAngle = this.direction;
+    this.coolDown = 30;
+    this.fireCooldown = 0.2;
+    console.log(x, y)
+  }
+
+  move() {
+    const size = Constants.MAP_SIZE;
+    if (this.x <= 300 || this.x >= size - 300 || this.y <= 300 || this.y >= size - 300) {
+      const mi = -this.direction
+      const ma = -this.direction + Math.PI / 4;
+      this.direction = Math.random() * (ma - mi) + mi;
+    }
+  }
+  
+ shot() {
+    const bulletArray = [];
+    if (this.coolDown < 0) this.coolDown = 30;
+    if (this.coolDown < 9) {
+      this.shootAngle += 10 * Math.PI / 180;
+      for (let i = 0;i < 4; i++) {
+        const bullet = new Bullet(this.id, this.x, this.y, this.shootAngle + i * Math.PI / 2, this.username);
+        bullet.speed = 250;
+        bulletArray.push(bullet);
+      }
+    }
+    this.coolDown -= 1;
+    return bulletArray;
+  }
+
+}
+
+module.exports = KuangCom;
--- src/server/computerplayer/index.js
+++ src/server/computerplayer/index.js
@@ -1,9 +1,11 @@
 const ChiSheng1 = require('./ChiSheng1');
 const ChiSheng2 = require('./ChiSheng2');
+const KuangCom = require('./KuangCom');
 
 module.exports = {
   // key: the id of the computer player.
   // value: the constructor function for each computerplayer.
   ChiSheng1,
   ChiSheng2,
+  KuangCom,
 }
